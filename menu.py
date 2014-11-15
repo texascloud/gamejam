0c1f71e7130e6afabebc1b9e2e6d97af47bb287e
Peter Mauldin <petermauldin1@gmail.com>
2014-11-15 15:32:25 -0600

    Finished menu

--- menu.py
+++ menu.py
@@ -1,5 +1,6 @@
 import sys, pygame
 from pygame.locals import *
+from game import main
 pygame.init()
 
 pygame.display.set_caption('Snake Solver')
@@ -7,60 +8,42 @@ size = width, height = 800, 600
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
+font = pygame.font.SysFont('Arial', 30)
 black = 0, 0, 0
 white = 255, 255, 255
 
 gameExit = False
-
 fps = 120
 
 while not gameExit:
 	clock.tick(fps)
+
+	#########TITLE#########
+	titleFont = pygame.font.SysFont('Arial', 60)
+	titleFont.set_bold
+	title = titleFont.render("Snake Solver", True, black)
+	title_rect = title.get_rect()
+
+	#########SUB-TITLE#########
+	subTitleFont = pygame.font.SysFont('Arial', 20)
+	subTitle = subTitleFont.render("(get_rect)", True, black)
+	sub_title_rect = subTitle.get_rect()
+
+	#########START#########
+	startFont = pygame.font.SysFont('Arial', 45)
+	start = startFont.render("Press 'Enter' to Play", True, (0, 0, 255))
+	start_rect = start.get_rect()
+
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
 		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LEFT and x_speed == 0:
-				x_speed = -snakeSize
-				y_speed = 0
-			if event.key == pygame.K_RIGHT and x_speed == 0:
-				x_speed = snakeSize
-				y_speed = 0
-			if event.key == pygame.K_UP and y_speed == 0:
-				x_speed = 0
-				y_speed = -snakeSize
-			if event.key == pygame.K_DOWN and y_speed == 0:
-				x_speed = 0
-				y_speed = snakeSize
-
-	x_pos += x_speed
-	y_pos += y_speed
-
-	for i in range(len(snake)-1):
-		snake[i] = snake[i+1]
-
-	
-	snake[len(snake)-1] = pygame.Rect(x_pos, y_pos, snakeSize, snakeSize)
-
-	if x_pos < 0 or x_pos > width:
-		gameOver("wall")
-		gameExit = True
-	if y_pos < 0 or y_pos > height:
-		gameOver("wall")
-		gameExit = True
+			if event.key == pygame.K_RETURN:
+				main()
 
 	screen.fill(white)
-	for i in range(len(snake)):
-		pygame.draw.rect(screen, black, snake[i], 0)
-	
-	for part in range(len(snake)-1):
-		idx = snake[len(snake)-1].colliderect(snake[part])
-		if idx != 0:
-			gameOver("self")
-			gameExit = True;
-		 
-	# idx = head.collidelist(snake)
-	# if(idx < 0):
-	# 	print idx
+	screen.blit(title, [width/2-(title_rect.w/2),height/4])
+	screen.blit(subTitle, [width/2-(sub_title_rect.w/2),height/3+ 15])
+	screen.blit(start, [width/2-(start_rect.w/2),height/2])
 
 	pygame.display.update()
