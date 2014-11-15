e6266e3e96cf467ce508f16b4684c105d847f2b8
Peter Mauldin <petermauldin1@gmail.com>
2014-11-15 11:13:16 -0600

    IDK

--- main.py
+++ main.py
@@ -10,14 +10,25 @@ black = 0, 0, 0
 white = 255, 255, 255
 
 gameExit = False
+start_x = 300
+start_y = 300
 snake = [0, 0, 50, 50]
 
 while not gameExit:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			gameExit = True
-		#elif event.type == KEYDOWN:
+		elif event.type == pygame.KEYDOWN:
+			if event.key == pygame.K_LEFT:
+				start_x -= 5
+			if event.key == pygame.K_RIGHT:
+				start_x += 5
+			if event.key == pygame.K_UP:
+				start_y -= 5
+			if event.key == pygame.K_DOWN:
+				start_y += 5	
+
 	screen.fill(white)
-	pygame.draw.rect(screen, black, snake, 0)
+	pygame.draw.rect(screen, black, [start_x, start_y, 5, 5], 0)
 	pygame.display.flip()
 
