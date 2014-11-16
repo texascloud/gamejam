73935c72af448f980b726d703cac1ea7662223f7
CamelBackNotation <j42hurt@gmail.com>
2014-11-15 22:50:17 -0600

    Finally fixed collision issues for rect objects

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-import sys, pygame, random, csv
+import sys, pygame, random, csv, eztext
 from operator import itemgetter
 from pygame.locals import *
 
@@ -12,6 +12,7 @@ def main(hardMode):
 	x_speed = snakeSize
 	y_speed = 0
 	newEquation = True
+	txtbx = eztext.Input(maxlength=45, color=(255,0,0), prompt='type here: ')
 
 
 	black = 0, 0, 0
@@ -42,11 +43,11 @@ def main(hardMode):
 	score = 0
 	fps = 20
 	font = pygame.font.SysFont('Arial', 30)
-	######################################### Generates random positions -- Should be list of Rects
+	############################################################# Generates random positions -- Should be list of Rects
 	def position_generator(amount):
 		numList = []
 		for i in range(amount):
-			applepos = pygame.Rect(  random.randint(img_size, width - img_size), random.randint(img_size + 60, height - img_size), img_size+5, img_size+5 )
+			applepos = pygame.Rect(  random.randint(img_size, width - (img_size+10)), random.randint(img_size + 50, height - img_size), img_size+10, img_size+10 )
 			#check if the applepos rect collides with the snake, if so generate a new one until it no longer collides
 			while applepos.collidelist(numList) != -1 or applepos.collidelist(snake) != -1:
 				applepos = pygame.Rect(  random.randint(img_size, width), random.randint(img_size, height), img_size, img_size )
@@ -99,7 +100,8 @@ def main(hardMode):
 		
 		scores = sorted(leaderboardList, key=itemgetter(1))
 		if end_score > scores[0][1]:
-			scores[0] = ("ZZZ", end_score)
+			#user_name = raw_input("Enter your name?")
+			scores[0] = ("der", end_score)
 
 		with open("scores.txt", "w") as f:
 		    csv.register_dialect("custom", delimiter=",", skipinitialspace=True)
